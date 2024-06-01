0b5132f8dc5e13c33c5fd06cf571ac469c2a3cd3
SypTitan <sybrentitan@gmail.com>
2024-06-01 22:58:55 +0200

    Turned the fox generation lua based

---
+++ fox.lua
@@ -0,0 +1,16 @@
+local img = get("foximg")
+
+local function setrandomfox(target)
+    local res = fetch({
+        url = "https://randomfox.ca/floof/",
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" },
+        body = ""
+    })
+
+    img.set_source(res["image"])
+end
+
+setrandomfox(img)
+
+get("newlink").set_href(window.link)

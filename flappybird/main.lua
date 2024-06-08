ede480e912602fd200213cb2ff4781245df7797b
SypTitan <sybrentitan@gmail.com>
2024-06-09 00:13:57 +0200

    added test print

--- flappybird/main.lua
+++ flappybird/main.lua
@@ -1,6 +1,8 @@
 print("Started script")
 -- import = require("https://raw.githubusercontent.com/SypTitan/webx-sites/main/importlib.lua")
 
+print("window.link: "..window.link)
+
 b64 = require("https://raw.githubusercontent.com/Reselim/Base64/master/Base64.lua")
 ppm = require(window.link .. "/ppmlib.lua")
 --ppm = import.get("ppmlib.lua")

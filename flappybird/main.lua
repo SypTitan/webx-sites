bbf38a930d31cb3e26f7eaefa2e73e61602217f0
SypTitan <sybrentitan@gmail.com>
2024-06-10 15:46:21 +0200

    Switched to modular encoding

--- flappybird/main.lua
+++ flappybird/main.lua
@@ -3,32 +3,35 @@ print("Started script")
 print("window.link: "..window.link)
 
 b64 = require("https://raw.githubusercontent.com/Reselim/Base64/master/Base64.lua")
-ppm = require("https://raw.githubusercontent.com/SypTitan/webx-sites/main/flappybird/ppmlib.lua")
---ppm = require(window.link.."/ppmlib.lua")
+--ppm = require("https://raw.githubusercontent.com/SypTitan/webx-sites/main/flappybird/ppmlib.lua")
+ppm = require(window.link.."/ppmlib.lua")
 
 local screen = get("screen")
 
 local score_board = get("score")
 local highscore_board = get("highscore")
 
+local tick_delay = 10
 local screen_width = 150
 local screen_height = 200
-local pipe_gap = 35
+local pipe_gap = 40
 local pipe_width = 35
+local pipe_speed = 3
 local buffered_click = false
 local bird_size = 17
+local bird_x = 15
 local bird_y = screen_height//3
 local bird_dy = 0
-local bird_grav = 2
-local bird_max_grav = 6
-local bird_jump = -4
+local bird_grav = 1.5
+local bird_max_grav = 5
+local bird_jump = -5
 local stop_game = true
 local score = 0
 local highscore = 0
 local prevtime = 0
 
 function pipeCollision(pipe_x: number, pipe_height: number): boolean
-    if (pipe_x < 15+bird_size and pipe_x+pipe_width > 15) then
+    if (pipe_x < bird_x+bird_size and pipe_x+pipe_width > bird_x) then
         if (bird_y < pipe_height or bird_y+bird_size > pipe_height+pipe_gap) then
             return true
         end
@@ -50,23 +53,35 @@ function tickGame(x: number, height: number)
     if buffered_click then
         buffered_click = false
         bird_dy = bird_jump
-        else
-            bird_dy = math.min(bird_max_grav, bird_dy+bird_grav)
-        end
+    else
+        bird_dy = math.min(bird_max_grav, bird_dy+bird_grav)
+    end
     bird_y = math.max(0, math.floor(bird_y+bird_dy))
     bird_y = math.min(screen_height-bird_size, bird_y)
-    
+    local physics = os.clock()
     -- First part of rendering
     local data = ppm.fillTable(screen_width, screen_height, 3, 252, 236)
+    local created = os.clock()
     ppm.setRectangleTable(data, current_x, 0, current_pipe_width, height, 0, 255, 0, screen_width, screen_height)
     ppm.setRectangleTable(data, current_x, height+pipe_gap, current_pipe_width, screen_height-(height+pipe_gap), 0, 255, 0, screen_width, screen_height)
-    ppm.setRectangleTable(data, 15, bird_y, bird_size, bird_size, 255,215,0, screen_width, screen_height)
-            
+    ppm.setRectangleTable(data, bird_x, bird_y, bird_size, bird_size, 255, 215, 0, screen_width, screen_height)
+    local rectangles = os.clock()            
+
     -- Final rendering
-    local final: string = table.concat(data, "\n")
-    local to_encode = buffer.fromstring(final)
-    local source = "data:image/x-portable-bitmap;base64," .. buffer.tostring(b64.encode(to_encode))
-    screen.set_source(source)
+    local final: string = table.concat(data)
+    -- print(string.sub(final, 1, 100))
+    local concatenated = os.clock()
+    screen.set_source(final)
+    local encoded = os.clock()
+
+    -- Displaying metrics
+    print("\n\n")
+    print("Physics: "..physics-start)
+    print("Background: "..created-physics)
+    print("Rectangles: "..rectangles-created)
+    print("Stringed: "..concatenated-rectangles)
+    print("Encoded: "..encoded-concatenated)
+    print("Total: "..encoded-start)
 
     -- Game loop
     if (stop_game) then
@@ -75,10 +90,10 @@ function tickGame(x: number, height: number)
     end
     if (bird_y+bird_size >= screen_height) then return end
     if (pipeCollision(x, height)) then return end
-    if (x > 5-pipe_width) then
-        set_timeout(function() tickGame(x-5, height) end, 50)
+    if (x > pipe_speed-pipe_width) then
+        set_timeout(function() tickGame(x-pipe_speed, height) end, tick_delay)
     else
-        set_timeout(function() tickGame(screen_width-5, math.random(screen_height//25, screen_height-(screen_height//25)-pipe_gap)) end , 50)
+        set_timeout(function() tickGame(screen_width-pipe_speed, math.random(screen_height//15, screen_height-(screen_height//15)-pipe_gap)) end , tick_delay)
         score += 1
         score_board.set_content("Score: "..score)
         if (score > highscore) then
@@ -92,10 +107,22 @@ function startGame()
     stop_game = false
     score = 0
     score_board.set_content("Score: 0")
+    bird_y = screen_height//3
+    bird_dy = 0
+    buffered_click = false
     tickGame(screen_width-3, math.random(screen_height//20, screen_height-(screen_height//20)-pipe_gap))
 end
 
-screen.set_source("data:image/x-portable-bitmap;base64," .. buffer.tostring(b64.encode(buffer.fromstring(ppm.fillFile(screen_width, screen_height, 3, 252, 236)))))
+-- local normal_method = buffer.tostring(b64.encode(buffer.fromstring(ppm.fillStringOg(screen_width, screen_height, 003, 252, 236))))
+local pre_encoded = ppm.fillString(screen_width, screen_height, 003, 252, 236)
+
+-- print(string.sub(normal_method, 1, 100))
+
+-- print(string.sub(pre_encoded, 1, 100))
+
+-- print("\nEqual: "..tostring(normal_method==pre_encoded))
+
+screen.set_source(pre_encoded)
 
 get("stop").on_click(function()
     stop_game = true
--- flappybird/ppmlib.lua
+++ flappybird/ppmlib.lua
@@ -1,25 +1,86 @@
 local ppmlib = {}
 
-function ppmlib.fillTable(width: number, height: number, red: number, green: number, blue: number): {string}
+local b64 = require("https://raw.githubusercontent.com/Reselim/Base64/master/Base64.lua")
+
+local preprocessed_colours = {}
+
+local encoded_header = { ["width"] = -1, ["height"] = -1, ["data"] = ""} 
+
+function getMeta(): string
+    return "data:image/x-portable-bitmap;base64,"
+end
+
+function createHeader(width: number, height: number): string
+    return "P3\n" .. width .. " " .. height .. "\n255\n"
+end
+
+function fixLength(data: string): string
+    return data..string.rep("\n", (3 - (string.len(data) % 3)) % 3)
+end
+
+function encode(data: string): string
+    return buffer.tostring(b64.encode(buffer.fromstring(fixLength(data))))
+end
+
+function processColour(colour: string): string
+    local processed_colour = preprocessed_colours[colour]
+    if (processed_colour == nil) then
+        processed_colour = encode(colour)
+        preprocessed_colours[colour] = processed_colour
+        print("Added colour "..colour .. ": " .. preprocessed_colours[colour])
+    end
+    return processed_colour
+end
+
+function encodeHeader(width: number, height: number): string
+    return encode(createHeader(width, height))
+end
+
+function getHeader(width: number, height: number): string
+    if (encoded_header["width"] == width and encoded_header["height"] == height) then
+        return encoded_header["data"]
+    else
+        encoded_header["width"] = width
+        encoded_header["height"] = height
+        encoded_header["data"] = encodeHeader(width, height)
+        return encoded_header["data"]
+    end
+end
+
+function ppmlib.fillStringOg(width: number, height: number, red: number, green: number, blue: number): string
+    return table.concat(ppmlib.fillTableOg(width, height, red, green, blue), "\n")
+end
+
+function ppmlib.fillTableOg(width: number, height: number, red: number, green: number, blue: number): {string}
     local colour = string.format("%d %d %d", red, green, blue)
-    local result = table.create(width*height+3, colour)
-    result[1] = "P3"
-    result[2] = width .. " " .. height
-    result[3] = 255
+    local result = table.create(width*height+1, colour)
+    result[1] = fixLength(createHeader(width, height))
 
     return result, width, height
 end
 
-function ppmlib.setRectangleTable(source: { string }, x: number, y: number, width: number, height: number, red: number, green: number, blue: number, fullwidth: number?, fullheight: number?): { string }
-    if (fullwidth == nil) then
-        local fullwidth: number = tonumber(string.split(source[2], " ")[1])
-    end
-    local offset: number = 3
+function ppmlib.fillString(width: number, height: number, red: number, green: number, blue: number): string
+    return table.concat(ppmlib.fillTable(width, height, red, green, blue), "")
+end
+
+function ppmlib.fillTable(width: number, height: number, red: number, green: number, blue: number): {string}
+    local colour = string.format("%d %d %d\n", red, green, blue)
+    local encoded_colour = processColour(colour)
+    local result = table.create(width*height+2, encoded_colour)
+    result[1] = getMeta()
+    result[2] = getHeader(width, height)
+
+    return result, width, height
+end
+
+function ppmlib.setRectangleTable(source: { string }, x: number, y: number, width: number, height: number, red: number, green: number, blue: number, fullwidth: number, fullheight: number?): { string }
+    local offset: number = 2
     local index: number = offset + x + y*fullwidth
-    local colour = string.format("%d %d %d", red, green, blue)
+    local colour = string.format("%d %d %d\n", red, green, blue)
+    local encoded_colour = processColour(colour)
     for i=1,height do
         for j=0,width-1 do
-            source[index+j] = colour
+            source[index+j] = encoded_colour
         end
         index += fullwidth
     end
